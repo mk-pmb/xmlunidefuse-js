2d19d0a5af414bd69d037a8283da89f3ed9544e7
mk-pmb <mk-pmb@users.noreply.github.com>
2016-08-30 00:18:58 +0200

    v0.1.2: [fix][readme] cdata mode != cdata encoding

--- test.js
+++ test.js
@@ -5,13 +5,13 @@
 var xud = require("xmlunidefuse"), eq = require("assert").strictEqual,
   raw = "<​!-- <u snow=\"man'>☃</u -->";
 
-eq(xud(raw),        //  HTML compat mode:    ...
+eq(xud(raw),        //  HTML compat mode:    v..
   "&lt;&#x200B;!-- &lt;u&#xA0;snow=&quot;man&#39;&gt;☃&lt;/u&#x205F;--&gt;");
 
-eq(xud.apos(raw),   //  apos opt-in:         ....
+eq(xud.apos(raw),   //  apos opt-in:         v...
   "&lt;&#x200B;!-- &lt;u&#xA0;snow=&quot;man&apos;&gt;☃&lt;/u&#x205F;--&gt;");
 
-//  CDATA encoding is implemented but not recommended,
+//  CDATA mode is implemented but not recommended,
 //  because it makes the sneaky chars VERY visible.
 eq(xud.cdata(raw),
   '<![CDATA[<]]>&#x200B;<![CDATA[!-- <u]]>&#xA0;<![CDATA[snow="ma' +
